8ee8b21670e9a764dacc6c67ff8f46bd9c33632c
Rodrigo Henrique <rodrigo.henrique@iatec.com>
2025-08-26 08:53:59 -0300

    feat: add transfer controller tests for POST /transfers endpoint

---
+++ test/controller/transferController.test.js
@@ -0,0 +1,27 @@
+//Bibliotecas
+const request = require('supertest'); // Iniciando o supetest
+const sinon = require('sinon'); //Iniciando o sinon
+const {expect} = require('chai');
+
+// Aplicação
+const app = require('../../app'); 
+
+// Testes
+describe('Transfer Controller', () => {
+    describe('POST /transfers', () => {
+        it('Quando informo remetente e destinatário inexistente recebo 400', async () => {
+            const resposta = await request(app)
+                .post('/transfer')
+                .send({ // o send envia como se fosse o json
+                    from: "rodrigo",
+                    to: "aline",
+                    amount: 100                    
+                    });// Vou usa o supertest apontando para o app
+
+            expect(resposta.status).to.equals(400);
+            // expect -> Espero a resposta
+
+        });
+    });
+
+});
