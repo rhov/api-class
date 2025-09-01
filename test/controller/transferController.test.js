5de53919d8f00c9aaa6414d06d21b63114aa49f2
Rodrigo Henrique <rodrigo.henrique@iatec.com>
2025-09-01 20:59:12 -0300

    fix: correct test case for POST /transfers and add missing GET /transfers description

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -19,9 +19,17 @@ describe('Transfer Controller', () => {
                     });// Vou usa o supertest apontando para o app
 
             expect(resposta.status).to.equals(400);
+            expect(resposta.body).
+                to.have.property('error', 'Usuário remetente ou destinatário não encontrado');
             // expect -> Espero a resposta
 
         });
     });
-    describe('GET ') 
+    describe('GET /transfers', () => {
+        it('Quando busco um GET retorno 200', async () =>{
+            const resposta = await request(app)
+                .get('/transfers');
+            expect(resposta.status).to.equals(200);
+        });
+    });
 });
