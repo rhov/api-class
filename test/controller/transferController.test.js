c28b2b89b4f3720f313c991a17464a227b2b07e6
Rodrigo Henrique <rodrigo.henrique@iatec.com>
2025-09-01 21:39:47 -0300

    test: add mock test for POST /transfers to validate error response

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -24,6 +24,22 @@ describe('Transfer Controller', () => {
             // expect -> Espero a resposta
 
         });
+
+        it('Usando Mocks: Quando informo remetente e destinatário inexistente recebo 400', async () => {
+            const resposta = await request(app)
+                .post('/transfer')
+                .send({ // o send envia como se fosse o json
+                    from: "alberto",
+                    to: "aline",
+                    amount: 100                    
+                    });// Vou usa o supertest apontando para o app
+
+            expect(resposta.status).to.equals(400);
+            expect(resposta.body).
+                to.have.property('error', 'Usuário remetente ou destinatário não encontrado');
+            // expect -> Espero a resposta
+
+        });
     });
     describe('GET /transfers', () => {
         it('Quando busco um GET retorno 200', async () =>{
