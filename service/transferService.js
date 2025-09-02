55baf0e35d867a3f66f421b70380fbc095fd698e
Rodrigo Henrique <rodrigo.henrique@iatec.com>
2025-09-01 22:28:43 -0300

    Criando validação de saldo insuficiente e teste mockado com sinon

--- service/transferService.js
+++ service/transferService.js
@@ -6,6 +6,10 @@ function transfer({ from, to, amount }) {
   const recipient = users.find(u => u.username === to);
   if (!sender || !recipient) throw new Error('Usuário remetente ou destinatário não encontrado');
   if (from === to) throw new Error('Não é possível transferir para si mesmo');
+  // Verifica se o saldo do remetente é suficiente
+  if (sender.balance < amount) {
+    throw new Error('Saldo insuficiente para realizar a transferência');
+  }
   if (!recipient.favorecido && amount >= 5000) {
     throw new Error('Transferências acima de R$ 5.000,00 só podem ser feitas para favorecidos');
   }
