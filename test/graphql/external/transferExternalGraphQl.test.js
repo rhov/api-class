5c9d3e0485acad81212b9bbf19a6a189ddd713f2
Rodrigo Henrique <rodrigo.henrique@iatec.com>
2025-09-14 16:45:08 -0300

    feat: update transfer tests to use createTransfer mutation and remove deprecated body.json

--- test/graphql/external/transferExternalGraphQl.test.js
+++ test/graphql/external/transferExternalGraphQl.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const { expect } = require('chai');
 const { apiURLGraphql, userLogin } = require('../../config/config');
 const { getToken } = require('../factory/requisições/login/loginUser')
-let token, mutation = require('../fixture/requisições/transfers/body.json');
+let token, createTransferMutation = require('../fixture/requisições/transfers/createTransfer.json');
 
 beforeEach(async () => {
     token = await getToken();
@@ -12,66 +12,32 @@ beforeEach(async () => {
 describe('Transfers External GraphQL', () => {
     it('Realizar Transferência com token obtido dinâmicamente', async () => {
 
-
         const respostaTransf = await request(apiURLGraphql)
             .post('')
             .set('Authorization', `Bearer ${token}`)
-            .send(mutation);
+            .send(createTransferMutation);
         expect(respostaTransf.status).to.equals(200);
     })
 
     it('Transferência com saldo insuficiente', async () => {
-
+        createTransferMutation.variables.amount = 9999999;
 
         const respostaTransf = await request(apiURLGraphql)
             .post('')
             .set('Authorization', `Bearer ${token}`)
-            .send({
-                query: `
-                        mutation Transfer($from: String!, $to: String!, $amount: Float!) {
-                            transfer(from: $from, to: $to, amount: $amount) {
-                                from
-                                to
-                                amount
-                                date
-                            }
-                        }
-                `,
-                variables: {
-                    from: "rodrigo",
-                    to: "aline",
-                    amount: 9999999
-                }
-
-            })
+            .send(createTransferMutation);
 
         expect(respostaTransf.body.errors[0].message).to.equals("Saldo insuficiente para realizar a transferência.");
     })
 
 
     it('Transferência para não favorefcido acima de 5k', async () => {
-
+        createTransferMutation.variables.amount = 5000.01;
+        createTransferMutation.variables.to = "sheldon";
         const respostaTransf = await request(apiURLGraphql)
             .post('')
             .set('Authorization', `Bearer ${token}`)
-            .send({
-                query: `
-                        mutation Transfer($from: String!, $to: String!, $amount: Float!) {
-                            transfer(from: $from, to: $to, amount: $amount) {
-                                from
-                                to
-                                amount
-                                date
-                            }
-                        }
-                `,
-                variables: {
-                    from: "rodrigo",
-                    to: "sheldon",
-                    amount: 5000.01
-                }
-
-            })
+            .send(createTransferMutation);
 
         expect(respostaTransf.body.errors[0].message).to.equals("Transferências acima de R$ 5.000,00 só podem ser feitas para favorecidos do remetente.");
     })
