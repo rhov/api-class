ab2999f4af3996553907f03e3f215d5fa8727f59
Rodrigo Henrique <rodrigo.henrique@iatec.com>
2025-09-14 17:00:10 -0300

    refactor: update transfer test setup to use local variable for createTransfer mutation

--- test/graphql/external/transferExternalGraphQl.test.js
+++ test/graphql/external/transferExternalGraphQl.test.js
@@ -2,16 +2,16 @@ const request = require('supertest');
 const { expect } = require('chai');
 const { apiURLGraphql, userLogin } = require('../../config/config');
 const { getToken } = require('../factory/requisições/login/loginUser')
-let token, createTransferMutation = require('../fixture/requisições/transfers/createTransfer.json');
-
-beforeEach(async () => {
-    token = await getToken();
-})
-
+const objetoCreate = require('../fixture/requisições/transfers/createTransfer.json');
 
 describe('Transfers External GraphQL', () => {
-    it('Realizar Transferência com token obtido dinâmicamente', async () => {
+    beforeEach(async () => {
+        token = await getToken();
+            createTransferMutation = JSON.parse(JSON.stringify(objetoCreate));
 
+    })
+
+    it('Realizar Transferência com token obtido dinâmicamente', async () => {
         const respostaTransf = await request(apiURLGraphql)
             .post('')
             .set('Authorization', `Bearer ${token}`)
@@ -21,7 +21,6 @@ describe('Transfers External GraphQL', () => {
 
     it('Transferência com saldo insuficiente', async () => {
         createTransferMutation.variables.amount = 9999999;
-
         const respostaTransf = await request(apiURLGraphql)
             .post('')
             .set('Authorization', `Bearer ${token}`)
@@ -32,14 +31,14 @@ describe('Transfers External GraphQL', () => {
 
 
     it('Transferência para não favorefcido acima de 5k', async () => {
+        console.log(createTransferMutation);
         createTransferMutation.variables.amount = 5000.01;
         createTransferMutation.variables.to = "sheldon";
         const respostaTransf = await request(apiURLGraphql)
             .post('')
             .set('Authorization', `Bearer ${token}`)
             .send(createTransferMutation);
-
-        expect(respostaTransf.body.errors[0].message).to.equals("Transferências acima de R$ 5.000,00 só podem ser feitas para favorecidos do remetente.");
+         expect(respostaTransf.body.errors[0].message).to.equals("Transferências acima de R$ 5.000,00 só podem ser feitas para favorecidos do remetente.");
     })
 
 });
